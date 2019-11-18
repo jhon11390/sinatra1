a076c168e4d919798160cd30e6f48334b1cae2e6
jhon11390 <meljhon2@gmail.com>
2019-11-18 04:21:36 -0500

    se inicia programa sinatra

---
+++ sinatra1.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+get '/' do
+    unless params[:nombre]
+        <<-HTML
+            <h1>Hola desconocido!</h1>
+        HTML
+    else
+        <<-HTML
+            <h1>"hola #{params[:nombre]}"</h1>
+        HTML
+        
+    end
+end
