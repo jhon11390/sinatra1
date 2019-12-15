2ea4a3f7cea267107fa960391a32982c093d5f9e
jhon11390 <meljhon2@gmail.com>
2019-12-15 16:42:17 -0500

    se cambia unless por if

--- solution.rb
+++ solution.rb
@@ -1,13 +1,13 @@
 require 'sinatra'
 
 get '/' do
-    unless params[:nombre]
-        <<-HTML
-            <h1>Hola desconocido!</h1>
+    if params[:nombre]
+         <<-HTML
+            <h1>Hola #{params[:nombre]}#{"!"}</h1>
         HTML
     else
-        <<-HTML
-            <h1>Hola #{params[:nombre]}#{"!"}</h1>
+       <<-HTML
+            <h1>Hola desconocido!</h1>
         HTML
     end
 end
