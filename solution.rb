b47a30d75f8e301d6ba9b3b7d8bbb0ab136f26a8
jhon11390 <meljhon2@gmail.com>
2019-12-17 04:29:37 -0500

    Se agrega la funcion length

--- solution.rb
+++ solution.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
 
 get '/' do
-    if params[:nombre]
+    if params[:nombre].length > 0
         "<h1>Hola #{params[:nombre]}!</h1>"
+    elsif params[:nombre].length == 0
+        "<h1>Hola desconocido!</h1>"
     else
         "<h1>Hola desconocido!</h1>"
     end
 end
-
