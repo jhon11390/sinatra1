cb23fc130135ff0433228ac3e8c5ffa1f8c640f1
Jhon Ortega <38445097+jhon11390@users.noreply.github.com>
2019-12-19 11:49:12 -0500

    se agrega path de ruta

--- solution.rb
+++ solution.rb
@@ -12,7 +12,19 @@ get '/' do
     end
 end
 
+get "/makers" do
+    "<h1>Hola desconocido!</h1>"
+end
+
+get "/makers/" do
+    "<h1>Hola desconocido!</h1>"
+end
+
 get '/makers/:nombre' do
     @nombre = params[:nombre].capitalize
-    "<h1>hola #{@nombre}!</h1>"
+    if @nombre.length > 0
+        "<h1>hola #{@nombre}!</h1>"
+    elsif @nombre.length == 0
+        "<h1>Hola desconocido!</h1>"
+    end
 end
